f42abc1badc8933903442b9eff7bb9ac2b0a58ec
choijinje <c74213060@gmail.com>
2026-03-19 17:21:50 +0900

    today last commit

---
+++ Step08_Input.py
@@ -0,0 +1,15 @@
+# Step08_Input.py
+
+# 콘솔창으로 부터 문자열 입력받기
+
+input_msg = input("메세지 입력 : ")
+
+print(f"입력한 메세지: {input_msg}")
+
+input_name: str = input("이름 입력 : ")
+input_addr: str = input("주소 입력 : ")
+
+print(f"이름 : {input_name}, 주소 : {input_addr}")
+
+# 문자열로 입력 받은 후
+input_age: str = input("나이 입력:")
